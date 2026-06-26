c9f762c0302be8a65556e64fcaa99a87322ab066
liuyujie714 <1051690424@qq.com>
2026-06-26 18:01:31 +0800

    build windows

--- src/cpp/system.cpp
+++ src/cpp/system.cpp
@@ -895,31 +895,29 @@ void System::process_reax_flow() {
         }
     }
 
-    for (const auto& pair : reaction_mol_id_pairs) {
+    for (const auto& pair : reaction_mol_id_pairs)
+    {
         intersection.clear();
 
         prev_mol = prev_sys->get_molecule_by_id(pair.first);
         curr_mol = this->get_molecule_by_id(pair.second);
 
-<<<<<<< HEAD:src/cpp/system.cpp
         const auto& smaller = prev_mol->atom_ids.size() < curr_mol->atom_ids.size()
             ? prev_mol->atom_ids
             : curr_mol->atom_ids;
         const auto& larger = prev_mol->atom_ids.size() < curr_mol->atom_ids.size()
             ? curr_mol->atom_ids
             : prev_mol->atom_ids;
-        for (int id : smaller) {
-            if (larger.count(id)) {
-=======
-        // Find atoms that are in both molecules.
-        for (int id : prev_mol->atom_ids) {
-            if (curr_mol->atom_ids.count(id)) {
->>>>>>> 5e7365d (fix set_intersection issue):cpp/system.cpp
+        for (int id : smaller)
+        {
+            if (larger.count(id))
+            {
                 intersection.push_back(id);
             }
         }
 
-        if (intersection.size() > 0) {
+        if (intersection.size() > 0)
+        {
             reax_flow->add_reaction(this->frame_id, intersection.size(), prev_mol, curr_mol);
         }
     }
