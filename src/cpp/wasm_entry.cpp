30be42c5eebb7aa1696e6b55e3b54257db17283e
liuyujie714 <1051690424@qq.com>
2026-08-05 21:26:40 +0800

    webpage

--- src/cpp/wasm_entry.cpp
+++
@@ -1,7 +0,0 @@
-#include <cstddef>
-
-int run_analysis(int argc, const char* const* argv);
-
-extern "C" int reax_run_analysis(int argc, const char** argv) {
-    return run_analysis(argc, argv);
-}
